23fcbab7531204528a3f001bb6d297ec5a5513b2
Seung Yeon <winningstreak91@gmail.com>
2017-01-29 01:34:16 +0900

    live coding change

--- src/containers/UserDashboard.js
+++ src/containers/UserDashboard.js
@@ -7,31 +7,162 @@ class UserDashboard extends Component {
   constructor() {
     super();
     this.state = {
-      data: null
+      userNum: null,
+      shakeNum: null,
+      useNum: null
     };
   }
 
   componentDidMount() {
     const rootRef = firebase.database().ref().child('profiles');
+    const couponRef = firebase.database().ref().child('couponDashboard');
+    const shakeRef = couponRef.child('shakeCount');
+    const useRef = couponRef.child('useCount');
+
     rootRef.on('value', snap => {
       const result = _.map(snap.val(), (val, uid) => {
         return { ...val, uid };
       });
-      this.setState({ data: result.length });
+      this.setState({ userNum: result.length });
+    });
+    shakeRef.on('value', snap => {
+      this.setState({ shakeNum: snap.val() });
+    });
+    useRef.on('value', snap => {
+      this.setState({ useNum: snap.val() });
     });
   }
 
   render() {
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          fillColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          strokeColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          pointColor: 'rgba(255, 140, 105, 1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(255, 140, 105, 1)',
+        },
+        {
+          label: '# of Votes',
+          data: [50, 20, 100, 40, 20, 10],
+          fillColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          strokeColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          pointColor: 'rgba(255, 140, 105, 1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(255, 140, 105, 1)',
+      },
+
+    ]
+    };
+    const lineData = {
+      labels: ['11', '12', '13', '14', '15', '16', '17'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3, 4],
+          fillColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          strokeColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          pointColor: 'rgba(255, 140, 105, 1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(255, 140, 105, 1)',
+        },
+        {
+          label: '# of Votes',
+          data: [50, 20, 100, 40, 20, 10, 2],
+          fillColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          strokeColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          pointColor: 'rgba(255, 140, 105, 1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(255, 140, 105, 1)',
+        },
+      ]
+    };
+    const option = {
+      options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+      }
+    };
+    
     return (
       <div>
         <h3>회원 대시보드</h3>
         <div>
           <h5>총 회원수</h5>
-          <p id="text_1">{this.state.data}명</p>
+          <p id="text_1">{this.state.userNum}명</p>
           <h5>총 쿠폰 발급횟수</h5>
-          <p id="text_1">125232번</p>
+          <p id="text_1">{this.state.shakeNum}번</p>
           <h5>총 쿠폰 사용횟수</h5>
-          <p id="text_1">12523번</p>
+          <p id="text_1">{this.state.useNum}번</p>
           <Bar data={data} options={option} width="600" height="250" />
           <Line data={lineData} options={option} width="600" height="250" />
         </div>
@@ -39,124 +170,5 @@ class UserDashboard extends Component {
     );
   }
 }
-
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      fillColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-      ],
-      strokeColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-      ],
-      pointColor: 'rgba(255, 140, 105, 1)',
-      pointStrokeColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(255, 140, 105, 1)',
-    },
-    {
-      label: '# of Votes',
-      data: [50, 20, 100, 40, 20, 10],
-      fillColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-      ],
-      strokeColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-      ],
-      pointColor: 'rgba(255, 140, 105, 1)',
-      pointStrokeColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(255, 140, 105, 1)',
-  },
-
-]
-};
-const lineData = {
-  labels: ['11', '12', '13', '14', '15', '16', '17'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3, 4],
-      fillColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-      ],
-      strokeColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-      ],
-      pointColor: 'rgba(255, 140, 105, 1)',
-      pointStrokeColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(255, 140, 105, 1)',
-    },
-    {
-      label: '# of Votes',
-      data: [50, 20, 100, 40, 20, 10, 2],
-      fillColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-      ],
-      strokeColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-      ],
-      pointColor: 'rgba(255, 140, 105, 1)',
-      pointStrokeColor: '#fff',
-      pointHighlightFill: '#fff',
-      pointHighlightStroke: 'rgba(255, 140, 105, 1)',
-    },
-  ]
-};
-const option = {
-  options: {
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    }
-  }
-};
 
 export default UserDashboard;
--- src/containers/UserListView.js
+++ src/containers/UserListView.js
@@ -1,9 +1,101 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+import { Loading } from '../components/common';
 
 class UserListView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userdata: null,
+      loading: false
+    };
+  }
+
+  componentWillMount() {
+    const rootRef = firebase.database().ref().child('profiles');
+    rootRef.on('value', snap => {
+      const result = _.map(snap.val(), (val, uid) => {
+        return { ...val, uid };
+      });
+
+      this.setState({ userdata: result });
+    });
+  }
+
+  userdataTableRender() {
+    let num = 1;
+    if (this.state.userdata) {
+      const { userdata } = this.state;
+      const result = userdata.map(data => {
+        return (
+          <tr className="table-none" key={data.uid}>
+            <th scope="row">{num++}</th>
+            <td>{data.displayName}</td>
+            <td>{data.email}</td>
+            <td>{data.provider ? data.provider : 'email'}</td>
+            <td>{data.couponCount ? data.couponCount : 0}</td>
+            <td>{data.couponUseCount ? data.couponUseCount : 0}</td>
+            <td>{data.gender && data.gender}</td>
+            <td>{data.emailVerified ? '인증' : '미인증'}</td>
+          </tr>
+        );
+      });
+      return <tbody>{result}</tbody>;
+    }
+    return <tbody>{null}</tbody>;
+  }
+
   render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
     return (
-      <div> UserListView </div>
+      <div className="container">
+        <h3>식당 리스트</h3>
+        <div className="navbar">
+          <button
+            className="btn btn-fixed"
+            // onClick={() => this.props.restaurantsLoad()}
+          >
+            새로고침
+          </button>
+          <button
+            className="btn btn-fixed"
+            // onClick={() => this.props.restaurantsLoad('categorize')}
+          >
+            분류순
+          </button>
+          <button
+            className="btn btn-fixed"
+            // onClick={() => this.props.restaurantsLoad('name')}
+          >
+            이름순
+          </button>
+          <button
+            className="btn btn-fixed"
+            // onClick={() => this.props.restaurantsLoad('register')}
+          >
+            등록순
+          </button>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>이름</th>
+              <th>이메일</th>
+              <th>제공사</th>
+              <th>흔든 횟수</th>
+              <th>사용 횟수</th>
+              <th>성별</th>
+              <th>인증</th>
+            </tr>
+          </thead>
+          {this.userdataTableRender()}
+        </table>
+      </div>
     );
   }
 }
